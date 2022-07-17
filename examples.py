541ee95fb86887336fbc9ec4014502e382b826c5
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-17 03:43:02 -0400

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,13 @@
+
+# Import (earlier version) of the shardHive EVM library
+from shardHiveEVM import sendEther, sendAltCoin
+
+# the shardHiveEVM program imports an Infura API URL via secretInfura.py
+# add your Infura URL to that program before running this script
+
+
+# sample python functions for easily transferring Ether and ERC-20 alt coins
+
+sendEthTest = sendEther(fromPublicKey, fromPrivateKey, toPublicKey, amount)
+
+sendAltCoin = sendAltCoin(fromPublicKey, fromPrivateKey, toPublicKey, altCoinAddress, Amount)
