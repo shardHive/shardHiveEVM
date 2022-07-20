f72b876599b305eb95a18960974ae9557d40abbe
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-19 23:23:39 -0400

    Create privateInfo.py

---
+++ privateInfo.py
@@ -0,0 +1,14 @@
+pKeys = {'shardHive2': 'privateKey2',
+         'shardHive1': 'privateKey1',
+         'shardHive0': 'privateKey0'}
+
+
+pubKeys = {'shardHive2': 'pubKey2',
+            'shardHive1': 'pubKey1',
+            'shardHive0': 'pubKey0'}
+
+
+infuraUrl = 'your ropsten infura URL'
+
+
+altCoinAddress = 'alt coin contract address'
