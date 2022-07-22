f2b40ab1b3864540e2ea465131199de1dd8c212e
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-22 08:17:49 -0400

    Update and rename privateInfo.py to secrets.py

---
+++ secrets.py
@@ -0,0 +1,14 @@
+pKeys = {'shardHive0': 'privateKey0',
+         'shardHive1': 'privateKey1',
+         'shardHive2': 'privateKey2'}
+
+
+pubKeys = {'shardHive0': 'pubKey0',
+            'shardHive1': 'pubKey1',
+            'shardHive2': 'pubKey2'}
+
+
+infuraUrl = 'your ropsten infura URL'
+
+
+currentShContract = 'alt coin contract address'
