56662313070c441c0224951f458f9e260407e08e
JB Larson <68449234+JBLarson@users.noreply.github.com>
2022-07-29 06:34:55 -0400

    Update shEvmTest.py

--- shEvmTest.py
+++ shEvmTest.py
@@ -2,13 +2,13 @@
 #!/usr/bin/python3
 
 from shardHiveEVM import *
-from secrets import pKeys, pubKeys, altCoinAddress
+from secrets import pKeys, pubKeys, currentShContract
 from datetime import datetime, timedelta, date
-from time import strftime, sleep
+from time import strftime
 import numpy as np
 
 
-shV0002contract = altCoinAddress
+shV0002contract = currentShContract
 
 #lambda functions for rounding
 ro1, ro2, ro4, ro6, ro8 = lambda x : round(x, 1), lambda x : round(x, 2), lambda x : round(x, 4), lambda x : round(x, 6), lambda x : round(x, 8)
@@ -35,20 +35,20 @@ sh0Addr, sh1Addr, sh2Addr = getAddrDict(sh0Pub), getAddrDict(sh1Pub), getAddrDic
 
 
 
-shV002totalSupply = fetchAltTotalSupply(shV0002contract)
+shV002totalSupply = fetchErc20TotalSupply(shV0002contract)
 
 
 sh0EthBal, sh1EthBal, sh2EthBal = fetchETHBalance(sh0Pub)['bal'], fetchETHBalance(sh1Pub)['bal'], fetchETHBalance(sh2Pub)['bal']
 
 
-sh0AltBal, sh1AltBal, sh2AltBal = fetchAltBalance(sh0Pub, shV0002contract), fetchAltBalance(sh1Pub, shV0002contract), fetchAltBalance(sh2Pub, shV0002contract)
+sh0Erc20Bal, sh1Erc20Bal, sh2Erc20Bal = fetchErc20Balance(sh0Pub, shV0002contract), fetchErc20Balance(sh1Pub, shV0002contract), fetchErc20Balance(sh2Pub, shV0002contract)
 
 
 
 
 
 # wallet sums
-sum01, sum02, sum12, sum012 = sh0AltBal + sh1AltBal, sh0AltBal + sh2AltBal, sh1AltBal + sh2AltBal, sh0AltBal + sh1AltBal + sh2AltBal
+sum01, sum02, sum12, sum012 = sh0Erc20Bal['bal'] + sh1Erc20Bal['bal'], sh0Erc20Bal['bal'] + sh2Erc20Bal['bal'], sh1Erc20Bal['bal'] + sh2Erc20Bal['bal'], sh0Erc20Bal['bal'] + sh1Erc20Bal['bal'] + sh2Erc20Bal['bal']
 sumDict = {'s01': sum01, 's02': sum02, 's12': sum12, 's012': sum012}
 
 print("Total Supply: " + str(shV002totalSupply))
@@ -58,36 +58,17 @@ for sumKey in list(sumDict.keys()):
 	print(str(sumKey) + '  ' + str(currentOwnership) + '%')
 
 
-print('\nsh0 Address: ' + str(sh0Addr['last']) + '  ETH: ' + str(ro4(sh0EthBal)) + '  sHive: ' + str(ro2(sh0AltBal)))
-print('sh1 Address: ' + str(sh1Addr['last']) + '  ETH: ' + str(ro4(sh1EthBal)) + '  sHive: ' + str(ro2(sh1AltBal)))
-print('sh2 Address: ' + str(sh2Addr['last']) + '  ETH: ' + str(ro4(sh2EthBal)) + '  sHive: ' + str(ro2(sh2AltBal)))
+print('\nsh0 Address: ' + str(sh0Addr['last']) + '  ETH: ' + str(ro4(sh0EthBal)) + '  sHive: ' + str(ro2(sh0Erc20Bal['bal'])))
+print('sh1 Address: ' + str(sh1Addr['last']) + '  ETH: ' + str(ro4(sh1EthBal)) + '  sHive: ' + str(ro2(sh1Erc20Bal['bal'])))
+print('sh2 Address: ' + str(sh2Addr['last']) + '  ETH: ' + str(ro4(sh2EthBal)) + '  sHive: ' + str(ro2(sh2Erc20Bal['bal'])))
 print('\n')
 
 
-try:
-	s12a = sendAltCoin(sh0Pub, sh0p, sh2Pub, shV0002contract, 120000)
-	print(s12a)
-except Exception as e:
-	print(e)
-
-print("\nSleep 12 seconds in between transactions\n")
-sleep(12)
-
-try:
-	s12b = sendAltCoin(sh1Pub, sh1p, sh2Pub, shV0002contract, 12000000)
-	print(s12b)
-except Exception as e:
-	print(e)
-
-
-
-sh0dict = {'walletAddress': sh0Pub, 'ethBal': sh0EthBal, 'altContract': shV0002contract, 'shBal': sh0AltBal}
-sh1dict = {'walletAddress': sh1Pub, 'ethBal': sh1EthBal, 'altContract': shV0002contract, 'shBal': sh1AltBal}
-sh2dict = {'walletAddress': sh2Pub, 'ethBal': sh2EthBal, 'altContract': shV0002contract, 'shBal': sh2AltBal}
+sh0dict = {'walletAddress': sh0Pub, 'ethBal': sh0EthBal, 'Erc20Contract': shV0002contract, 'shBal': sh0Erc20Bal['bal']}
+sh1dict = {'walletAddress': sh1Pub, 'ethBal': sh1EthBal, 'Erc20Contract': shV0002contract, 'shBal': sh1Erc20Bal['bal']}
+sh2dict = {'walletAddress': sh2Pub, 'ethBal': sh2EthBal, 'Erc20Contract': shV0002contract, 'shBal': sh2Erc20Bal['bal']}
 
 shUsers = [sh0dict, sh1dict, sh2dict]
-
-
 
 
 #for user in shUsers: print(user)
